bd2b12c7fce41603ca8537239a975d7850f787e6
thaip <thaip@thaiphuc>
2024-01-07 22:30:14 +0700

    Test Commit 1

--- src/Controller/BankMain.java
+++ src/Controller/BankMain.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package view_controller;
+package Controller;
 
 import View.Menu;
 import java.util.Scanner;
